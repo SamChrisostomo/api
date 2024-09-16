c986a9479cdf05a4b1dfbc55728c6513d6764892
Samuel Crisóstomo <samueld162@gmail.com>
2024-09-16 14:34:52 -0300

    Criado função de serviço para autenticação de usuário utilizando JWT.

---
+++ services/AuthPerson.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const { createHmac } = require("node:crypto");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+exports.getHash = () => {
+    let hash;
+
+    const hmac = createHmac('sha256', process.env.JWT_PASS);
+    hmac.on('readable', () => {
+        const data = hmac.read();
+        if (data) {
+            hash = data.toString('hex');
+            const HashFile = path.join(__dirname, "hash.txt");
+            fs.writeFileSync(HashFile, hash);
+        };
+    });
+
+    hmac.end();
+
+    return hash;
+}
+
+exports.getProfile = (person) => {
+    let newPerson = {
+        name: person.name,
+        bio: person.biography,
+        avatar: person.avatar,
+        email: person.email,
+        phone: person.phone,
+    }
+
+    return newPerson;
+}
+
+exports.authPerson = async (token) => {
+    const decoded = await jwt.verify(token, this.getHash());
+    return decoded;
+}
